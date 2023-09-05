b71e25bac4ec612e3c02967265bbdf6a098379f0
LoicBurriRochon <2322244@cmontmorency.qc.ca>
2023-09-05 11:20:49 -0400

    timer and result file base

--- FicheTechnique/Form2.Designer.cs
+++ FicheTechnique/Form2.Designer.cs
@@ -29,11 +29,68 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.timerBattery = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lbInitialCharge = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.Location = new System.Drawing.Point(218, 35);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(379, 64);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "Battery Tester";
+            this.lbTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // timerBattery
+            // 
+            this.timerBattery.Interval = 1800000;
+            this.timerBattery.Tick += new System.EventHandler(this.timerBattery_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(185, 388);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lbInitialCharge
+            // 
+            this.lbInitialCharge.AutoSize = true;
+            this.lbInitialCharge.Location = new System.Drawing.Point(257, 193);
+            this.lbInitialCharge.Name = "lbInitialCharge";
+            this.lbInitialCharge.Size = new System.Drawing.Size(13, 13);
+            this.lbInitialCharge.TabIndex = 2;
+            this.lbInitialCharge.Text = "0";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbInitialCharge);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lbTitle);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.Timer timerBattery;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Label lbInitialCharge;
     }
 }
--- FicheTechnique/Form2.cs
+++ FicheTechnique/Form2.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management;
+using System.IO;
+using System.CodeDom;
 
 namespace FicheTechnique
 {
@@ -15,6 +18,73 @@ namespace FicheTechnique
         public Form2()
         {
             InitializeComponent();
+        }
+
+
+        //file path for the battery test result text file. this is the active user directory
+        public static string filename = @"C:\Users\"+Environment.GetEnvironmentVariable("Username")+"\\Battery_Test_Result.txt";
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            DialogResult dialogResult = MessageBox.Show("This test will register the device's battery percentage every 30 min in a text file until the computer shuts down. Try running multiple tasks so the test is more accurate. Do you want to proceed", "Battery Test", MessageBoxButtons.YesNo);
+            if(dialogResult == DialogResult.Yes)
+            {
+               //fetch Battery informations
+                ManagementObjectSearcher mos = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Battery");
+                //gets the estimated charge remaining in the battery to display it into a label
+                foreach (ManagementObject mo in mos.Get())
+                {
+
+                   
+                    lbInitialCharge.Text = mo["EstimatedChargeRemaining"].ToString() + "%";
+                }
+
+                try
+                {
+
+                    //creates the text file and writes the title
+                    using (StreamWriter sw = new StreamWriter(filename, true))
+                    {
+                        sw.WriteLine("Battery Test Results");
+                        sw.Close();
+                    }
+                    
+
+
+                }
+                catch (Exception Ex)
+                {
+                    Console.WriteLine(Ex.ToString());
+                }
+                //timer start
+                timerBattery.Enabled = true;
+                timerBattery.Start();
+
+            }
+
+
+
+        }
+
+        private void timerBattery_Tick(object sender, EventArgs e)
+        {
+            //fetch battery's estimated charge remaining according to the system and writes it into the result text file
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Battery");
+
+            foreach(ManagementObject mo in mos.Get())
+            {
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(filename, true))
+                    {
+                        sw.WriteLine(mo["EstimatedChargeRemaining"].ToString() + "%");
+                        sw.Close();
+                    }
+                }catch(Exception Ex)
+                {
+                    Console.WriteLine(Ex.ToString());
+                }
+            }
         }
     }
 }
